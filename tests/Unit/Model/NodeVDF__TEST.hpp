e6498c8e8f9af4702e8fb67b9598000a8b4f71ff
daniel-v-k <daniel.kretschmer@posteo.de>
2022-12-02 16:23:03 +0100

    unit tests for VDF ready to be written

--- tests/Unit/Model/NodeVDF__TEST.hpp
+++ tests/Unit/Model/NodeVDF__TEST.hpp
@@ -3,12 +3,13 @@
 
 using NodeVector = std::shared_ptr<std::vector<std::unique_ptr<GlobalFlow::Model::NodeInterface>>>;
 
-class StandardNodeFixture : public ::testing::Test {
+class StandardNodeVDFFixture : public ::testing::Test {
 public:
     NodeVector nodes;
+    DensityProperties densityProperties;
 
     void SetUp() {
-        DensityProperties densityProperties = GlobalFlow::Model::DensityProperties::setDensityProperties(true, {1000.0, 1012.5, 1025.0}, 0.2, 0.2);
+        densityProperties = GlobalFlow::Model::DensityProperties::setDensityProperties(true, {1000.0, 1012.5, 1025.0}, 0.2, 0.2);
 
         NodeVector ptr(new vector <unique_ptr<GlobalFlow::Model::NodeInterface>>);
         nodes = std::move(ptr);
@@ -45,28 +46,28 @@ public:
         nodes->at(3)->setNeighbour(1, TOP);
 
         // zeta surface 0 (all at top of nodes)
-        nodes->at(0)->addInitialZeta(10.0 * si::meter, 0);
-        nodes->at(1)->addInitialZeta(10.0 * si::meter, 0);
-        nodes->at(2)->addInitialZeta(0.0 * si::meter, 0);
-        nodes->at(3)->addInitialZeta(0.0 * si::meter, 0);
+        nodes->at(0)->addInitialZeta(10.0 * si::meter);
+        nodes->at(1)->addInitialZeta(10.0 * si::meter);
+        nodes->at(2)->addInitialZeta(0.0 * si::meter);
+        nodes->at(3)->addInitialZeta(0.0 * si::meter);
 
         // zeta surface 1 (in nodes 0 & 1 between, in nodes 2 & 3 at top)
-        nodes->at(0)->addInitialZeta(7.5 * si::meter, 0);
-        nodes->at(1)->addInitialZeta(2.5 * si::meter, 1);
-        nodes->at(2)->addInitialZeta(0.0 * si::meter, 2);
-        nodes->at(3)->addInitialZeta(0.0 * si::meter, 3);
+        nodes->at(0)->addInitialZeta(7.5 * si::meter);
+        nodes->at(1)->addInitialZeta(2.5 * si::meter);
+        nodes->at(2)->addInitialZeta(0.0 * si::meter);
+        nodes->at(3)->addInitialZeta(0.0 * si::meter);
 
         // zeta surface 2 (in nodes 0 & 1 at bottom, in nodes 2 & 3 between)
-        nodes->at(0)->addInitialZeta(0.0 * si::meter, 0);
-        nodes->at(1)->addInitialZeta(0.0 * si::meter, 1);
-        nodes->at(2)->addInitialZeta(-2.5 * si::meter, 2);
-        nodes->at(3)->addInitialZeta(-7.5 * si::meter, 3);
+        nodes->at(0)->addInitialZeta(0.0 * si::meter);
+        nodes->at(1)->addInitialZeta(0.0 * si::meter);
+        nodes->at(2)->addInitialZeta(-2.5 * si::meter);
+        nodes->at(3)->addInitialZeta(-7.5 * si::meter);
 
         // zeta surface 3 (all at bottom of nodes)
-        nodes->at(0)->addInitialZeta(0.0 * si::meter, 0);
-        nodes->at(1)->addInitialZeta(0.0 * si::meter, 1);
-        nodes->at(2)->addInitialZeta(-10.0 * si::meter, 2);
-        nodes->at(3)->addInitialZeta(-10.0 * si::meter, 3);
+        nodes->at(0)->addInitialZeta(0.0 * si::meter);
+        nodes->at(1)->addInitialZeta(0.0 * si::meter);
+        nodes->at(2)->addInitialZeta(-10.0 * si::meter);
+        nodes->at(3)->addInitialZeta(-10.0 * si::meter);
     }
 
     using p_node = std::unique_ptr<GlobalFlow::Model::NodeInterface>;
@@ -74,11 +75,11 @@ public:
     p_node &at(int pos) { return nodes->at(pos); }
 };
 
-TEST_F(StandardNodeFixture, setZetaPosInZone) {
+TEST_F(StandardNodeVDFFixture, setZetaPosInZone) {
 
 }
 
-TEST_F(StandardNodeFixture, getRHSConstantDensity) {
+TEST_F(StandardNodeVDFFixture, getRHSConstantDensity) {
     at(0)->setHead_direct(1);
     at(0)->addExternalFlow(RECHARGE, 0, 50, 0);
     at(0)->addExternalFlow(RIVER, 1 * si::meter, 50, 1 * si::meter);
@@ -86,120 +87,121 @@ TEST_F(StandardNodeFixture, getRHSConstantDensity) {
     ASSERT_EQ((at(0)->getRHSConstantDensity().value()), -50);
 }
 
-TEST_F(StandardNodeFixture, getRHS) {
+TEST_F(StandardNodeVDFFixture, getZoneConductance) {
+    unordered_map<NeighbourPosition, large_num> neighbourList = at(0)->getListOfNeighbours();
+    std::unordered_map<NeighbourPosition, large_num>::const_iterator got = neighbourList.find(RIGHT);
+    ASSERT_EQ((at(0)->getZoneConductances(got)[0].value()), 0); // todo compute correct result
+}
+
+TEST_F(StandardNodeVDFFixture, getRHS) {
     at(0)->setHead_direct(1);
     at(0)->addExternalFlow(RECHARGE, 0, 50, 0);
     at(0)->addExternalFlow(RIVER, 1 * si::meter, 50, 1 * si::meter);
     at(0)->addExternalFlow(WETLAND, 1 * si::meter, 50, 1 * si::meter);
 
-    // todo calculate correct result
-    ASSERT_EQ((at(0)->getRHS().value()), 0);
+    ASSERT_EQ((at(0)->getRHS().value()), 0); // todo calculate correct result
 }
 
-TEST_F(StandardNodeFixture, getZetaRHS) {
-
+TEST_F(StandardNodeVDFFixture, getZetaRHS) {
+    at(0)->getZetaRHS(1);
 }
 
-TEST_F(StandardNodeFixture, getEffectivePorosityTerm) {
-
-}
-
-TEST_F(StandardNodeFixture, getZetaConductannce) {
+TEST_F(StandardNodeVDFFixture, getEffectivePorosityTerm) {
 
 }
 
-TEST_F(StandardNodeFixture, getNusTop) {
+TEST_F(StandardNodeVDFFixture, getZetaConductannce) {
 
 }
 
-TEST_F(StandardNodeFixture, getNusBot) {
+TEST_F(StandardNodeVDFFixture, getNusTop) {
 
 }
 
-TEST_F(StandardNodeFixture, getSourceTermBelowZeta) {
+TEST_F(StandardNodeVDFFixture, getNusBot) {
 
 }
 
-TEST_F(StandardNodeFixture, getTipToeFlow) {
+TEST_F(StandardNodeVDFFixture, getSourceTermBelowZeta) {
 
 }
 
-TEST_F(StandardNodeFixture, getZoneConductance) {
+TEST_F(StandardNodeVDFFixture, getTipToeFlow) {
 
 }
 
-TEST_F(StandardNodeFixture, getZoneConductanceCum) {
+TEST_F(StandardNodeVDFFixture, getZoneConductanceCum) {
 
 }
 
-TEST_F(StandardNodeFixture, getFlowPseudoSource) {
+TEST_F(StandardNodeVDFFixture, getFlowPseudoSource) {
 
 }
 
-TEST_F(StandardNodeFixture, getVerticalFluxCorrection) {
+TEST_F(StandardNodeVDFFixture, getVerticalFluxCorrection) {
 
 }
 
-TEST_F(StandardNodeFixture, getFluxCorrTop) {
+TEST_F(StandardNodeVDFFixture, getFluxCorrTop) {
 
 }
 
-TEST_F(StandardNodeFixture, getFluxCorrDown) {
+TEST_F(StandardNodeVDFFixture, getFluxCorrDown) {
 
 }
 
-TEST_F(StandardNodeFixture, clipTopZetasToHeads) {
+TEST_F(StandardNodeVDFFixture, clipTopZetasToHeads) {
 
 }
 
-TEST_F(StandardNodeFixture, verticalZetaMovement) {
+TEST_F(StandardNodeVDFFixture, verticalZetaMovement) {
 
 }
 
-TEST_F(StandardNodeFixture, horizontalZetaMovement) {
+TEST_F(StandardNodeVDFFixture, horizontalZetaMovement) {
 
 }
 
-TEST_F(StandardNodeFixture, clipZetaHeights) {
+TEST_F(StandardNodeVDFFixture, clipZetaHeights) {
 
 }
 
-TEST_F(StandardNodeFixture, correctCrossingZetas) {
+TEST_F(StandardNodeVDFFixture, correctCrossingZetas) {
 
 }
 
-TEST_F(StandardNodeFixture, preventZetaLocking) {
+TEST_F(StandardNodeVDFFixture, preventZetaLocking) {
 
 }
 
-TEST_F(StandardNodeFixture, addInitialZeta) {
+TEST_F(StandardNodeVDFFixture, addInitialZeta) {
 
 }
 
-TEST_F(StandardNodeFixture, setZeta) {
+TEST_F(StandardNodeVDFFixture, setZeta) {
 
 }
 
-TEST_F(StandardNodeFixture, getZetas) {
+TEST_F(StandardNodeVDFFixture, getZetas) {
 
 }
 
-TEST_F(StandardNodeFixture, getZetasChange) {
+TEST_F(StandardNodeVDFFixture, getZetasChange) {
 
 }
 
-TEST_F(StandardNodeFixture, updateZetaChange) {
+TEST_F(StandardNodeVDFFixture, updateZetaChange) {
 
 }
 
-TEST_F(StandardNodeFixture, setZoneOfSinksAndSources) {
+TEST_F(StandardNodeVDFFixture, setZoneOfSinksAndSources) {
 
 }
 
-TEST_F(StandardNodeFixture, setEffectivePorosity) {
+TEST_F(StandardNodeVDFFixture, setEffectivePorosity) {
 
 }
 
-TEST_F(StandardNodeFixture, setEffectivePorosity_direct) {
+TEST_F(StandardNodeVDFFixture, setEffectivePorosity_direct) {
 
 }
